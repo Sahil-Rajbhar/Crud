259bd767a676cafe35a4ef320b9a7695d6ca6347
Sahil-Rajbhar <sahil.rajbhar@canvascraft.media>
2022-08-24 09:56:57 +0530

    @include directive is used

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -1,31 +1,6 @@
 @component('layouts.app')
-    <div class="create-container">
-        <center><h1 class="heading">Adding new Employee</h1></center>
-        <br>            
-        {{ Form::open(array('route'=>'employees.store')) }}
-            {{ Form::token() }}
-            <div class="form-group">    
-                {{ Form::label('name', 'name') }} &nbsp; 
-                {{ Form::text('name') }}
-                @if ($errors->has('name'))
-                    <ul>
-                        <li><span>{{ $errors->first('name') }}</span></li>
-                    </ul>
-                @endif
-                <br>
-                <br>
-                {{ Form::label('email', 'email') }} &nbsp;
-                {{ Form::email('email') }}
-                @if ($errors->has('email'))
-                    <ul>
-                        <li><span>{{ $errors->first('email') }}</span></li>
-                    </ul>
-                @endif 
-                <br> 
-                <br>
-                <a href="/employees" class="create-link">Cancel</a> &nbsp;
-                {{ Form::submit('Add', ['class' => 'bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded']) }}
-            </div>  
-        {{ Form::close() }}
+    <center><h1 class="heading">Adding new Employee</h1></center>
+    <div class="edit-container">
+        @include('forms.employee_form')
     </div>
 @endcomponent
--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -1,36 +1,6 @@
 @component('layouts.app')
     <center><h1 class="edit-title">Updating Employee Data</h1></center>
     <div class="edit-container">
-    {{ Form::open(array('route' => array('employees.update', $employee->id), 'method'=>'put'))}}
-        {{ Form::token() }}
-        <table>
-            <tr>
-                <td>{{ Form::label('name','name') }}</td>
-                <td>{{ Form::text('name', $employee->name) }}<br><br>
-                    @if ($errors->has('name'))
-                        <ul>
-                            <li><span>{{ $errors->first('name') }}</span></li>
-                        </ul>
-                    @endif
-                </td>
-            </tr>
-            <tr>
-                <td>{{ Form::label('email','email') }}</td>
-                <td>{{ Form::text('email', $employee->email) }}<br><br>
-                    @if ($errors->has('email'))
-                        <ul>
-                            <li><span class="text-danger">{{ $errors->first('email') }}</span></li>
-                        </ul>
-                    @endif
-                </td>
-            </tr>
-            <tr>
-                <td><a href="/employees" class="cancel-edit">Cancel</a></td>
-                <td>
-                    {{ Form::submit('Update', ['class' => 'bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded']) }}
-                </td>
-            </tr>
-        </table>    
-    {{Form::close()}}
+        @include('forms.employee_form', $employee)
     </div>
 @endcomponent
